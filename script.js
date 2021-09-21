7d636780d6520913b9fb32a9bf2ff58601f6431a
Hiran Whites <61298965+Hirtes@users.noreply.github.com>
2021-09-21 09:57:44 -0300

    Atualização de arquivos

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ function recu_pass() {
     //Ainda não programado
     alert("funcionando")
 }
+
+function relogio(){
+    //capturando os valores para o relogio
+    var data = new Date()
+    var hour = data.getHours()
+    var min = data.getMinutes()
+    var sec = data.getSeconds()
+    //condições para colocar o número 0 nos valores menor que 10
+    if (hour<10) {
+        hour = "0"+hour;
+    }
+    if (min<10) {
+        min = "0"+min;
+    }
+    if (sec<10) {
+        sec = "0"+sec;
+    }
+    //Variavel para formatação hora:minuto:segundo
+    var time = (hour + ":" + min + ":" + sec);
+    //Alterando o valor da tag legend no html para exibir a hora
+    document.getElementById("hora").innerHTML = time;
+    /*
+    var yourlegend = document.getElementById("hora");
+    yourlegend.innerHTML = time;
+    */
+    }
+    //variavel para chamar a função relogio a cada 1 segundo
+    var timer=setInterval(relogio,1000);
+
+function logout() {
+    document.location = "index.html"
+}
