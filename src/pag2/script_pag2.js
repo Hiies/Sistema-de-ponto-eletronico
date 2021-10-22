50a3c5d4f9e8fcb1519f13485b83e2fa48bed698
Hiran Whites <hiran.whites@hotmail.com>
2021-10-21 23:13:53 -0300

    Atualizando JS
    Criando funções para utilizar futuramente

--- src/pag2/script_pag2.js
+++ src/pag2/script_pag2.js
@@ -1,12 +1,20 @@
-function relogio(){
-    //capturando os valores para o relogio
-    var data = new Date()
-    var hour = data.getHours()
+function FullData() {
+    //Capturando data e criando Array para definir o mês/Dia da semana de acordo com a lingua pt-br
+    var data = new Date();
+    var mes = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+    var semana = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
+    return (semana[data.getDay()] + ", " + data.getDate() + " de " + mes[data.getMonth()] + " de " + data.getFullYear())
+}
+
+function hora() {
+    //Capturando hora
+    var data = new Date();
+    var hora = data.getHours()
     var min = data.getMinutes()
     var sec = data.getSeconds()
-    //condições para colocar o número 0 nos valores menor que 10
-    if (hour<10) {
-        hour = "0"+hour;
+    //Condição para adicionar o "0" na frente de número abaixo de 10
+    if (hora<10) {
+        hora = "0" + hora;
     }
     if (min<10) {
         min = "0"+min;
@@ -14,18 +22,18 @@ function relogio(){
     if (sec<10) {
         sec = "0"+sec;
     }
-    //Variavel para formatação hora:minuto:segundo
-    var time = (hour + ":" + min + ":" + sec);
-    //Alterando o valor da tag legend no html para exibir a hora
-    document.getElementById("hora").innerHTML = time;
-    /*
-    var yourlegend = document.getElementById("hora");
-    yourlegend.innerHTML = time;
-    */
+    return(hora + ":" + min + ":" + sec);
+}
+//Função para exibir relogio
+function relogio(){
+    //variavel para capturar os valores de função hora()
+    var getHora = hora()
+    //innerHTML para trocar o valor da legenda para exibir o relogio na tela
+    document.getElementById("hora").innerHTML = getHora;
     }
-    //variavel para chamar a função relogio a cada 1 segundo
-    var timer=setInterval(relogio,1000);
+//Variavel para atulizar a função relogio a cada 1 segundo
+var timer=setInterval(relogio,1000);
 
 function logout() {
-    document.location = "../login/index.html"
+   document.location = "../login/index.html"
 }
